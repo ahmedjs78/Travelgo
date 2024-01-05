fa063287560a79e304964a07fa6c9e99dbc370e9
ahmedjhin <a7madti900@gmail.com>
2024-01-05 19:28:21 +0400

    custom buttons add

--- src/sectionf.jsx
+++ src/sectionf.jsx
@@ -1,32 +1,49 @@
+
 // import function to register Swiper custom elements
 import { register } from 'swiper/element/bundle';
 import "./css/sectionf.css"
 
-// import Swiper core and required modules
-import {FreeMode,Autoplay, Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 import "./css/swpre.css";
 
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 // register Swiper custom elements
 register();
 import "swiper/css"
 
-  
+
 
 const Sfslider = () => {
+    const swiper = useSwiper()
     return(
         <>
-        
+        <div className='secf-cont'>
+            <Swiper 
+                    css-mode="true"
+                    FreeMode={true}
+                    navigation={{ prevEl: '.prev', nextEl: '.next',}} 
+                    spaceBetween={30}
+                    slidesPerView={3}
+                    centeredSlides={true}
+                    scrollbar={{ draggable: true }}
+                    onSwiper={(swiper) => console.log(swiper)}
+                    onSlideChange={() => console.log('slide change')}>
+                <SwiperSlide>contenst</SwiperSlide>
+                <SwiperSlide>contenst</SwiperSlide>
+                <SwiperSlide>contenst</SwiperSlide>
+                <SwiperSlide>contenst</SwiperSlide>
+                <SwiperSlide>contenst</SwiperSlide>
+                <SwiperSlide>contenst</SwiperSlide>
+                <SwiperSlide>contenst</SwiperSlide>
+            </Swiper>
+            
+                <div className='butons-con'>
+                <div className="prev" onClick={() => swiper.slidePrev()}/>
+                <div className="next" onClick={() => swiper.slideNext()}/>
+                </div>
+        </div>
         </>
-
     )
-}
+};
 
 export default Sfslider
