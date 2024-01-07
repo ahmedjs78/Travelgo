525dd0804e9957a03cc9581b91553d1af7b92d6f
ahmedjhin <a7madti900@gmail.com>
2024-01-08 02:45:03 +0400

    working on the other slider

--- src/sectionf.jsx
+++ src/sectionf.jsx
@@ -18,54 +18,60 @@ const Sfslider = () => {
     return(
         <>
         <div className='secf-cont'>
-            <Swiper 
-                    css-mode="true"
-                    FreeMode={true}
-                    navigation={{ prevEl: '.prev', nextEl: '.next',}} 
-                    spaceBetween={30}
-                    slidesPerView={3}
-                    centeredSlides={true}
-                    scrollbar={{ draggable: true }}
-                    onSwiper={(swiper) => console.log(swiper)}
-                    onSlideChange={() => console.log('slide change')}>
-                <SwiperSlide>
-                    <div className='card'>
-                        <div className='card-padding'>
-                            <div>
-                                <img src="src\assets\cards\booking 1.png" alt="" />
-                            </div>
-                            <div className='card-p2'>
-                                <h1 className='card-h1'>Easy Booking</h1>
-                                <p className='card-p'>Square, was moving across the sand in their direction.</p>
-                            </div>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className='card'>
-                        <div><img src="src\assets\cards\booking 1.png" alt="" /></div>
-                        <div>
-                            <h1>Easy Booking</h1>
-                            <p>Square, was moving across the sand in their direction.</p>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className='card'>
-                        <div><img src="src\assets\cards\booking 1.png" alt="" /></div>
-                        <div>
-                            <h1>Easy Booking</h1>
-                            <p>Square, was moving across the sand in their direction.</p>
-                        </div>
-                    </div>
-                </SwiperSlide>
-            </Swiper>
-            
-                {/* <div className='butons-con'>
-                <div className="prev" onClick={() => swiper.slidePrev()}/>
-                <div className="next" onClick={() => swiper.slideNext()}/>
-                </div> */}
+
+        <div className=''>
+
         </div>
+            <div className=''>
+                <Swiper 
+                        css-mode="true"
+                        FreeMode={true}
+                        navigation={{ prevEl: '.prev', nextEl: '.next',}} 
+
+                        slidesPerView={2}
+                        centeredSlides={true}
+                        scrollbar={{ draggable: true }}
+                        onSwiper={(swiper) => console.log(swiper)}
+                        onSlideChange={() => console.log('slide change')}>
+                    <SwiperSlide className="testf">
+                        <div className='card'>
+                            <div className='card-padding'>
+                                <div>
+                                    <img src="src\assets\cards\booking 1.png" alt="" />
+                                </div>
+                                <div className='card-p2'>
+                                    <h1 className='card-h1'>Easy Booking</h1>
+                                    <p className='card-p'>Square, was moving across the sand in their direction.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className='card'>
+                            <div><img src="src\assets\cards\booking 1.png" alt="" /></div>
+                            <div>
+                                <h1>Easy Booking</h1>
+                                <p>Square, was moving across the sand in their direction.</p>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className='card'>
+                            <div><img src="src\assets\cards\booking 1.png" alt="" /></div>
+                            <div>
+                                <h1>Easy Booking</h1>
+                                <p>Square, was moving across the sand in their direction.</p>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                </Swiper>
+                
+                    <div className='butons-con'>
+                        <div className="prev" onClick={() => swiper.slidePrev()}><img src="src\assets\cards\arrow-left-solid.svg"></img></div>
+                        <div className="next" onClick={() => swiper.slideNext()}><img src='src\assets\cards\arrow-right-solid.svg'></img></div>
+                    </div>
+            </div>
+    </div>
         </>
     )
 };
