a9f2961852e1e7c12530b7b267ff70c3a405bf5e
ahmedjhin <ahmedjhin7@gmail.com>
2024-01-30 16:16:31 +0400

    secc left side almost don

--- src/sectionc.jsx
+++ src/sectionc.jsx
@@ -2,16 +2,17 @@ const Sectionc = () => {
     return (
         <>
         <div className="mt-[110px] flex flex-row gap-[42px] pl-20">
-            <div className="w-[696px] h-[696px] bg-[#f2f3]">
-                <div className="relative top-[105px] right-[-72px] w-auto">
+            <div className="relative w-[696px] h-[696px]">
+                <div className="absolute top-[105px] left-[72px] w-auto z-[2]">
                     <img src="src\assets\sectionc\Ellipse 15.svg" alt="" />
                 </div>
-                <div className="relative top-[20] left-[491px]">
+                <div className="absolute  top-[20] right-[109px]">
                     <img src="src\assets\sectionc\Ellipse 14.svg" alt="" />
                 </div>
-                <div className="relative left-[400px] z-[2] sadow2 bg-[#fff] w-[302px] h-[72px] rounded-full flex gap-3 items-center justify-center"> <img className="w-[40px] h-[40px]" src="src\assets\sectionc\discount 1.svg"/><p className="text-[23px] text-[#191825] font-['circular']"> Discounted Price</p></div>
-                <div className="relative top-[-200px] z-[2]"> <img src="src\assets\sectionc\My project 1.png" alt="" /></div>
-                <div className="relative top-[-770px] z-[1] w-[750px]"><img src="src\assets\sectionc\Rectangle 7.png" alt="" /></div>
+                <div className="absolute  left-[400px] z-[2] top-[131px] sadow2 bg-[#fff] w-[302px] h-[72px] rounded-full flex gap-3 items-center justify-center"> <img className="w-[40px] h-[40px]" src="src\assets\sectionc\discount 1.svg"/><p className="text-[23px] text-[#191825] font-['circular']"> Discounted Price</p></div>
+                <div className="absolute  z-[2]"> <img src="src\assets\sectionc\My project 1.png" alt="" /></div>
+                
+                <div className="rounded-r-[1130px] absolute right-[70px] top-[129px] w-[705px] h-[550px] bg-[#FACD49] z-[1]"></div>
             </div>
             <div className="w-[445px] h-[667px] bg-[#d21]"></div>
         </div>
